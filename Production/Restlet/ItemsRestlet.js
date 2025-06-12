6be2150902353c6ba6a0164eb9ac097b92a1e7c4
Luis Barrios <lguillermobp@gmail.com>
2025-06-12 09:03:24 -0400

    first commit

---
+++ Production/Restlet/ItemsRestlet.js
@@ -0,0 +1,81 @@
+"use strict";
+
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Restlet
+ * @NModuleScope SameAccount
+ */
+define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.js"],
+    /**
+     * @param {N/search} search
+     * @param {N/record} record
+     * @return {{post: exports.post}}
+     */
+    function (search, record, log, GENERALTOOLS) {
+
+         function get (_ref) {
+
+            return _ref;
+        }
+
+         function post (context) {
+
+            log.debug("context",context);
+            try {
+            contextjson = JSON.parse(context);
+            }
+            catch (e) {
+                log.debug("error",e);
+                contextjson = context;
+            }
+
+            
+            var option= contextjson.option;
+            log.debug("option",option); 
+
+           
+            idsearch = "customsearch_ecd_items";
+
+
+             try {
+                var fsearch =search.load({
+                            id: idsearch
+                    });
+                log.debug({ title: 'Search loaded successfully', details: fsearch });
+
+                } catch (error) {
+                log.error({ title: 'Error loading saved search', details: error });
+                }
+                
+             var pagedData = fsearch.runPaged({
+                "pageSize" : 1000
+            });
+            var i=0;
+            var dataf=[];
+            pagedData.pageRanges.forEach(function (pageRange) {
+                log.audit(pageRange.index);
+                var page = pagedData.fetch({index: pageRange.index});
+                page.data.forEach(function (fresult1) {
+                    
+                    dataf[i] = {
+                        "bin_id": 0,
+                        "item_id": Number(fresult1.getValue({name: "internalid"})),
+                        "item_name": fresult1.getValue({name: "itemid"}),
+                        "item_description": fresult1.getValue({name: "purchasedescription"}),
+                        "item_unit": fresult1.getText({name: "unitstype"})
+                    }
+                    i++;
+
+                })
+            })
+
+            return dataf;
+        }
+
+
+        return {
+            get: get,
+            post: post
+
+        };
+    });
