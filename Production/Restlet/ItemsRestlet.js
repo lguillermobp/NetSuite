b608d215c985672397738ccb35bc7b28efd64002
Luis Barrios <lguillermobp@gmail.com>
2025-07-16 20:32:17 -0400

    first commit

--- Production/Restlet/ItemsRestlet.js
+++ Production/Restlet/ItemsRestlet.js
@@ -46,6 +46,26 @@ define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.j
                 } catch (error) {
                 log.error({ title: 'Error loading saved search', details: error });
                 }
+
+                   if (option == "U") {
+
+                var defaultFilters = fsearch.filters;
+                var customFilters = [];
+                
+                customFilters = {
+                    name: "lastmodifieddate",
+                    operator: "within",
+                    values: "today",
+                    isor: false,
+                    isnot: false,
+                    leftparens: 0,
+                    rightparens: 0
+
+                };
+
+                defaultFilters.push(customFilters);
+                fsearch.filters = defaultFilters;           
+                }
                 
              var pagedData = fsearch.runPaged({
                 "pageSize" : 1000
