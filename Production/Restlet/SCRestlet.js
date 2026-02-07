35f3363efb1dc901e761e53d77e69fa6380b7f94
Luis Barrios <lguillermobp@gmail.com>
2026-02-06 20:33:06 -0500

    first commit

--- Production/Restlet/SCRestlet.js
+++ Production/Restlet/SCRestlet.js
@@ -5,7 +5,7 @@
  * @NScriptType Restlet
  * @NModuleScope SameAccount
  */
-define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.js"],
+define(["N/search", "N/record", "N/log", "/SuiteScripts/Modules/generaltoolsv1.js"],
     /**
      * @param {N/search} search
      * @param {N/record} record
@@ -13,82 +13,83 @@ define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.j
      */
     function (search, record, log, GENERALTOOLS) {
 
-         function get (_ref) {
+        function get(_ref) {
 
             return _ref;
         }
 
-         function post (context) {
+        function post(context) {
 
             contextjson = JSON.parse(context);
-            log.debug("context",contextjson);
-            var option= contextjson.option;
-            log.debug("option",option);
+            log.debug("context", contextjson);
+            var option = contextjson.option;
+            log.debug("option", option);
 
-           
+
             idsearch = "customsearch_ecdsalescontract_2";
 
-            var fsearch =search.load({
-               id: idsearch
-           });
-                
-             var pagedData = fsearch.runPaged({
-                "pageSize" : 1000
+            var fsearch = search.load({
+                id: idsearch
             });
-            var i=0;
-            var dataf=[];
+
+            var pagedData = fsearch.runPaged({
+                "pageSize": 1000
+            });
+            var i = 0;
+            var dataf = [];
             pagedData.pageRanges.forEach(function (pageRange) {
                 log.audit(pageRange.index);
-                var page = pagedData.fetch({index: pageRange.index});
+                var page = pagedData.fetch({ index: pageRange.index });
                 page.data.forEach(function (fresult1) {
 
-                    if (!fresult1.getValue({name: "custbody_lpr_startdate"})) {
-                        startdatelpr='01/01/0001';
+                    if (!fresult1.getValue({ name: "custbody_lpr_startdate" })) {
+                        startdatelpr = '01/01/0001';
                     }
                     else {
-                        startdatelpr = fresult1.getValue({name: "custbody_lpr_startdate"});
+                        startdatelpr = fresult1.getValue({ name: "custbody_lpr_startdate" });
                     }
-                    if (!fresult1.getValue({name: "custbody_lpr_enddate"})) {
-                        enddatelpr='01/01/0001';
-                    }
-                    else {
-                        enddatelpr = fresult1.getValue({name: "custbody_lpr_enddate"});
-                    }
-                    if (!fresult1.getValue({name: "custbody_lpr_enddatawaterfall"})) {
-                        enddatebasedwaterfall='01/01/0001';
+                    if (!fresult1.getValue({ name: "custbody_lpr_enddate" })) {
+                        enddatelpr = '01/01/0001';
                     }
                     else {
-                        enddatebasedwaterfall = fresult1.getValue({name: "custbody_lpr_enddatawaterfall"});
+                        enddatelpr = fresult1.getValue({ name: "custbody_lpr_enddate" });
                     }
-                    if (!fresult1.getValue({name: "custbody_invoicedate"})) {
-                        invoicedate='01/01/0001';
+                    if (!fresult1.getValue({ name: "custbody_lpr_enddatawaterfall" })) {
+                        enddatebasedwaterfall = '01/01/0001';
                     }
                     else {
-                        invoicedate = fresult1.getValue({name: "custbody_invoicedate"});
+                        enddatebasedwaterfall = fresult1.getValue({ name: "custbody_lpr_enddatawaterfall" });
+                    }
+                    if (!fresult1.getValue({ name: "custbody_invoicedate" })) {
+                        invoicedate = '01/01/0001';
+                    }
+                    else {
+                        invoicedate = fresult1.getValue({ name: "custbody_invoicedate" });
                     }
 
 
                     dataf[i] = {
-                        "internalid": fresult1.getValue({name: "internalid"}),
-                        "salescontract_name": fresult1.getValue({name: "transactionnumber"}),
-                        "customer_id": fresult1.getValue({name: "entity"}),
-                        "amount": fresult1.getValue({name: "amount"}),
-                        "productionline": fresult1.getText({name: "custbody_productionline"}),
-                        "productionlineid": fresult1.getValue({name: "custbody_productionline"}),
-                        "clientOnHold": fresult1.getValue({name: "custbody_clientinhold"}),
-                        "sc_date": fresult1.getValue({name: "trandate"}),
+                        "internalid": fresult1.getValue({ name: "internalid" }),
+                        "salescontract_name": fresult1.getValue({ name: "transactionnumber" }),
+                        "customer_id": fresult1.getValue({ name: "entity" }),
+                        "amount": fresult1.getValue({ name: "amount" }),
+                        "productionline": fresult1.getText({ name: "custbody_productionline" }),
+                        "productionlineid": fresult1.getValue({ name: "custbody_productionline" }),
+                        "clientOnHold": fresult1.getValue({ name: "custbody_clientinhold" }),
+                        "sc_date": fresult1.getValue({ name: "trandate" }),
                         "sc_date_invoice": invoicedate,
-                        "make": fresult1.getText({name: "custbody_appf_make_ecd"}),
-                        "model": fresult1.getText({name: "custbody_appf_veh_model"}),
-                        "vin_id": fresult1.getValue({name: "custbody_vin"}),
+                        "make": fresult1.getText({ name: "custbody_appf_make_ecd" }),
+                        "model": fresult1.getText({ name: "custbody_appf_veh_model" }),
+                        "vin_id": fresult1.getValue({ name: "custbody_vin" }),
                         "sc_startdateLPR": startdatelpr,
                         "sc_enddateLPR": enddatelpr,
-                        "memo": fresult1.getValue({name: "memo"}),
-                        "sc_balance": Number(fresult1.getValue({name: "custbody_ecd_balance"})),
-                        "sc_paid": Number(fresult1.getValue({name: "custbody_ecd_amountpaid"})),
+                        "memo": fresult1.getValue({ name: "memo" }),
+                        "sc_balance": Number(fresult1.getValue({ name: "custbody_ecd_balance" })),
+                        "sc_paid": Number(fresult1.getValue({ name: "custbody_ecd_amountpaid" })),
                         "sc_enddatebasedwaterfall": enddatebasedwaterfall,
                         "status_lpr": fresult1.getValue(fresult1.columns[27]),
-                        "sc_bay": fresult1.getText({name: "custbody_bay"})
+                        "sc_bay": fresult1.getText({ name: "custbody_bay" }),
+                        "sc_bay_id": Number(fresult1.getValue({ name: "custbody_bay" })),
                     }
                     i++;
 
--- Production/Restlet/ScheduleRestlet.js
+++ Production/Restlet/ScheduleRestlet.js
@@ -5,7 +5,7 @@
  * @NScriptType Restlet
  * @NModuleScope SameAccount
  */
-define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.js"],
+define(["N/search", "N/record", "N/log", "/SuiteScripts/Modules/generaltoolsv1.js"],
     /**
      * @param {N/search} search
      * @param {N/record} record
@@ -13,78 +13,79 @@ define(["N/search", "N/record",  "N/log","/SuiteScripts/Modules/generaltoolsv1.j
      */
     function (search, record, log, GENERALTOOLS) {
 
-         function get (_ref) {
+        function get(_ref) {
 
             return _ref;
         }
 
-         function post (context) {
+        function post(context) {
 
             try {
-            contextjson = JSON.parse(context);
+                contextjson = JSON.parse(context);
             }
             catch (e) {
-                log.debug("error",e);
+                log.debug("error", e);
                 contextjson = context;
             }
-            log.debug("context",contextjson);
-            var option= contextjson.option;
-            log.debug("option",option);
+            log.debug("context", contextjson);
+            var option = contextjson.option;
+            log.debug("option", option);
 
-           
+
             idsearch = "customsearch_ecd_schedule";
 
-            var fsearch =search.load({
-               id: idsearch
-           });
-                
+            var fsearch = search.load({
+                id: idsearch
+            });
+
             if (option == "U") {
 
                 var defaultFilters = fsearch.filters;
                 var customFilters = [];
-                
-                    customFilters = {
-                        name: "lastmodifieddate",
-                        operator: "within",
-                        values: "today",
-                        isor: false,
-                        isnot: false,
-                        leftparens: 0,
-                        rightparens: 0
 
-                    };
+                customFilters = {
+                    name: "lastmodifieddate",
+                    operator: "within",
+                    values: "today",
+                    isor: false,
+                    isnot: false,
+                    leftparens: 0,
+                    rightparens: 0
 
-                    defaultFilters.push(customFilters);
-                    fsearch.filters = defaultFilters;           
-                }
+                };
 
-             var pagedData = fsearch.runPaged({
-                "pageSize" : 1000
+                defaultFilters.push(customFilters);
+                fsearch.filters = defaultFilters;
+            }
+
+            var pagedData = fsearch.runPaged({
+                "pageSize": 1000
             });
-            var i=0;
-            var dataf=[];
+            var i = 0;
+            var dataf = [];
             pagedData.pageRanges.forEach(function (pageRange) {
                 log.audit(pageRange.index);
-                var page = pagedData.fetch({index: pageRange.index});
+                var page = pagedData.fetch({ index: pageRange.index });
                 page.data.forEach(function (fresult1) {
 
                     dataf[i] = {
-                        "productionline_id": Number(fresult1.getValue({name: "custrecord_sc_productionline", join: "CUSTRECORD_SO_SC_TASK"})),
-                        "productionline_name": fresult1.getText({name: "custrecord_sc_productionline", join: "CUSTRECORD_SO_SC_TASK"}),
-                        "task_id": Number(fresult1.getValue({name: "internalid"})),
-                        "tasks_id": Number(fresult1.getValue({name: "internalid", join: "CUSTRECORD_SO_SC_TASK"})),
-                        "task_name": fresult1.getValue({name: "custrecord_sc_task", join: "CUSTRECORD_SO_SC_TASK"}),
-                        "task_seq": Number(fresult1.getValue({name: "custrecord_sc_tasksseq", join: "CUSTRECORD_SO_SC_TASK"})),
-                        "task_sts": fresult1.getText({name: "custrecord_so_sc_status"}),
-                        "task_note": fresult1.getValue({name: "custrecord_so_sc_note"}),
-                        "task_startdate": fresult1.getValue({name: "custrecord_so_sc_startdate"}),
-                        "task_enddate": fresult1.getValue({name: "custrecord_so_sc_enddate"}),
-                        "task_duration": fresult1.getValue({name: "custrecord_sc_soduration"}),
-                        "salescontract_id": Number(fresult1.getValue({name: "internalid",   join: "CUSTRECORD_SALECONTRACT"})),
-                        "group_by": fresult1.getText({name: "custrecord_sc_tasksgroup",join: "CUSTRECORD_SO_SC_TASK"}),
-                        "progress": Number(fresult1.getValue({name: "custrecord_so_sc_progress"})),
-                        "lastmodified_by": fresult1.getValue({name: "custrecord_so_sc_user"}),
-                        "sc_bay": fresult1.getText({name: "custbody_bay", join: "CUSTRECORD_SALECONTRACT"})
+                        "productionline_id": Number(fresult1.getValue({ name: "custrecord_sc_productionline", join: "CUSTRECORD_SO_SC_TASK" })),
+                        "productionline_name": fresult1.getText({ name: "custrecord_sc_productionline", join: "CUSTRECORD_SO_SC_TASK" }),
+                        "task_id": Number(fresult1.getValue({ name: "internalid" })),
+                        "tasks_id": Number(fresult1.getValue({ name: "internalid", join: "CUSTRECORD_SO_SC_TASK" })),
+                        "task_name": fresult1.getValue({ name: "custrecord_sc_task", join: "CUSTRECORD_SO_SC_TASK" }),
+                        "task_seq": Number(fresult1.getValue({ name: "custrecord_sc_tasksseq", join: "CUSTRECORD_SO_SC_TASK" })),
+                        "task_sts": fresult1.getText({ name: "custrecord_so_sc_status" }),
+                        "task_note": fresult1.getValue({ name: "custrecord_so_sc_note" }),
+                        "task_startdate": fresult1.getValue({ name: "custrecord_so_sc_startdate" }),
+                        "task_enddate": fresult1.getValue({ name: "custrecord_so_sc_enddate" }),
+                        "task_duration": fresult1.getValue({ name: "custrecord_sc_soduration" }),
+                        "salescontract_id": Number(fresult1.getValue({ name: "internalid", join: "CUSTRECORD_SALECONTRACT" })),
+                        "group_by": fresult1.getText({ name: "custrecord_sc_tasksgroup", join: "CUSTRECORD_SO_SC_TASK" }),
+                        "progress": Number(fresult1.getValue({ name: "custrecord_so_sc_progress" })),
+                        "lastmodified_by": fresult1.getValue({ name: "custrecord_so_sc_user" }),
+                        "sc_bay": fresult1.getText({ name: "custbody_bay", join: "CUSTRECORD_SALECONTRACT" }),
+                        "sc_bay_id": Number(fresult1.getValue({ name: "custbody_bay", join: "CUSTRECORD_SALECONTRACT" })),
                     }
                     i++;
 
